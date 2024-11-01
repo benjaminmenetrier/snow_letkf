f07b3868156e419c26de66f2ea4ebf24327e3a95
Benjamin Menetrier <benjamin.menetrier@met.no>
2024-11-01 09:33:36 +0100

    Only OOPS-JEDI requested

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 # Parameters
 
-# Directory for ECMWF OOPS binaries
-ecbin=${HOME}/build/oops-bundle/bin
-
 # Directory for JEDI OOPS binaries
 jedibin=${HOME}/build/jedi-bundle/bin
+
+# Directory for ECMWF OOPS binaries
+#ecbin=${HOME}/build/oops-bundle/bin
 
 # Number of tasks for OOPS
 ntasks=4
@@ -14,11 +14,11 @@ ntasks=4
 # Run
 
 # Get test data
-curl https://www.dropbox.com/scl/fi/rs8f8k7xzyy964ocd9ug8/data.tar.gz?rlkey=lg3vguxctuzwuidmx1o0nh7m2 -o data.tar.gz -J -L -k  
+#curl https://www.dropbox.com/scl/fi/rs8f8k7xzyy964ocd9ug8/data.tar.gz?rlkey=lg3vguxctuzwuidmx1o0nh7m2 -o data.tar.gz -J -L -k  
 tar -xvzf data.tar.gz
 
 # Create grid coordinates
-mpirun -n ${ntasks} ${ecbin}/saber_quench_convertstate.x json/grid.json
+mpirun -n ${ntasks} ${jedibin}/saber_quench_convertstate.x json/grid.json
 
 # Format observations
 python python/formatObservations.py
@@ -28,8 +28,11 @@ python python/formatBackgrounds.py
 
 # Run HofX for each member
 for mem in $(seq -f "%06g" 0 20); do
+#  sed -e s/%mem%/${mem}/g json/ec_hofx.json > json/hofx_${mem}.json
+#  mpirun -n 1 ${ecbin}/saber_quench_hofx.x json/hofx_${mem}.json
   sed -e s/%mem%/${mem}/g json/hofx.json > json/hofx_${mem}.json
-  mpirun -n 1 ${ecbin}/saber_quench_hofx.x json/hofx_${mem}.json
+  mpirun -n 1 ${jedibin}/quenchxx_hofx3d.x json/hofx_${mem}.json
+  rm -f json/hofx_${mem}.json
 done
 
 # Gather HofX
